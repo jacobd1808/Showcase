716b5a8ea815d99ade7dbe39585d8975ddf24bf2
jacobd1808 <jacobd1808@yahoo.co.uk>
2016-01-25 23:52:07 +0000

    register

--- app/views/modules/profileDelete.php
+++
@@ -1,3 +0,0 @@
-<h3> Are you sure you want to delete this family member profile? </h3>
-<a href='' class='cir disable closePopup'> <i class="material-icons">close</i> </a>
-<a href='' class='cir disable confirmDelete'> <i class="material-icons">check</i> </a>
--- register.php
+++ register.php
@@ -14,20 +14,26 @@
 	);
 
   if (isset($_POST["register"])) { 
+    // Store all Data in array to pass to Model
     $data = array(
       'username' => $_POST["username"], 
       'name' => $_POST["name"],
       'email' => $_POST["email"],
       'password' => md5($_POST["password"]),
+      'gender' => $_POST["email"],
     );
 
+    // Validation 
     if (!isset($data['username']) || !isset($data['name']) || !isset($data['email']) || !isset($data['password'])) { 
       $feedback = array( "type" => 'error', "message" => '<i class="material-icons">error</i><em>You must fill in all the fields</em>' ); 
     } else { 
+      // If Validation passes, send data to controller
       $newUser = $profiles->createUser($data);
       if (!$newUser) { 
-      $feedback = array( "type" => 'error', "message" => '<i class="material-icons">error</i><em>Something went wrong</em>' ); 
+        // If Something goes wrong 
+        $feedback = array( "type" => 'error', "message" => '<i class="material-icons">error</i><em>Something went wrong</em>' ); 
       } else { 
+        // In Works (Need icon changing)
         $feedback = array( "type" => 'confirm', "message" => '<i class="material-icons">error</i><em>Successfully Registered, <a href="login.php"> Log in</a></em>' );  
       }
     }
@@ -41,7 +47,10 @@
     </head>
     <body id='pre-login'>
       <div class='view'> 
-        <!-- --> 
+        <!-- 
+          Username, Email, Password, First Name, Surname, Gender 
+          Validation for everything
+        --> 
       </div>
     	<?php include_once "app/views/scripts.php"; ?>
     </body>
