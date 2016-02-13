7d121edce6e1d81a82d32e11ae5d2e470baf6a79
jacobd1808 <jacobd1808@yahoo.co.uk>
2016-02-13 01:45:44 +0000

    Navigation

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php 
 
-  include_once "app/config/checkSession.php";
-  include_once "app/config/conn.php";
-  include "app/controller/indexController.php";
+  //include_once "app/config/checkSession.php";
+  //include_once "app/config/conn.php";
+  //include "app/controller/indexController.php";
 	
 	$pageOpt = array(
 		"title"			    =>	"Welcome", 
@@ -19,7 +19,24 @@
     <body>
       <?php include_once "app/views/header.php"; ?>
       <div class='view'> 
-          Testing 
+        <header>
+          <nav>
+              <ul class='navigation-menu'>
+                  <li><a href="#"><i class="material-icons">featured_play_list</i> Feed </a></li>
+                  <li><a href='#'><i class="material-icons">search</i> Search </a> </li>
+                  <li><a href="#"><i class="material-icons">sentiment_very_satisfied</i> Friends </a> </li>
+                  <li><a href="#"><i class="material-icons">group</i>Groups</a></li>
+                  <li><a href="#" class='de-emphasis'><i class="material-icons">play_for_work</i>Logout</a></li>
+              </ul>
+              <div class='user-profile-tab'> 
+                <a href='#'> <i class="material-icons">account_circle</i> Jacob Dickinson </a>
+              </div>
+          </nav>
+          <div class='clear'></div>
+        </header>
+        <div class='information-menu'>
+        ABC
+        </div>
       </div>
     	<?php include_once "app/views/scripts.php"; ?>
     </body>
