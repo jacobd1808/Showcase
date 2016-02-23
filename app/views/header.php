59c2a35dc3c15ecbbd4c198e2e2759130de351c4
jacobd1808 <jacobd1808@yahoo.co.uk>
2016-02-23 01:08:06 +0000

    nav

--- app/views/header.php
+++ app/views/header.php
@@ -3,19 +3,29 @@
   // This just sets active menu 
   function setActive($nav, $page){ 
     if ($nav == $page) { 
-      echo 'theme-colour-lighten';
+      echo 'active';
     } 
   }
 ?>
 
-<!-- 
-<div id="sidr" class='theme-colour-main'>
-  <ul>
-    <li><a href="index.php" class='<?php echo setActive('home', $pageOpt['navName']); ?>'>Home</a></li>
-   <li><a href="diagnoseProfiles.php" class='<?php echo setActive('track', $pageOpt['navName']); ?>'>Diagnose</a></li>
-    <li><a href="info.php" class='<?php echo setActive('info', $pageOpt['navName']); ?>'>Learn</a></li>
-    <li><a href="tips.php" class='<?php echo setActive('tips', $pageOpt['navName']); ?>'>Top Tips</a></li>
-    <li><a href="logout.php">Logout</a></li>
-  </ul>
-</div>
--->
+<header>
+  <nav>
+      <ul class='navigation-menu'>
+          <li><a href="index.php" class='main-link <?= setActive('feed', $pageOpt['navName']); ?>'><i class="material-icons">featured_play_list</i> Feed </a></li>
+          <li><a href='search.php' class='main-link <?= setActive('search', $pageOpt['navName']); ?>'><i class="material-icons">search</i> Search </a> </li>
+          <li><a href="friends.php" class='main-link <?= setActive('friends', $pageOpt['navName']); ?>'><i class="material-icons">sentiment_very_satisfied</i> Friends </a> </li>
+          <li><a href="groups.php" class='main-link <?= setActive('groups', $pageOpt['navName']); ?>'><i class="material-icons">group</i>Groups</a></li>
+      </ul>
+      <div class='user-profile-tab'> 
+        <a href='profile.php' class='main-link <?= setActive('profile', $pageOpt['navName']); ?>'> Jacob Dickinson <i class="material-icons">account_circle</i> </a>
+        <ul class='user-menu'> 
+            <li> <a href='message_center.php' class='<?= setActive('messages', $pageOpt['navName']); ?>'> Messages <i class="material-icons">message</i> </a></li>
+            <li> <a href='notification_center' class='<?= setActive('notifications', $pageOpt['navName']); ?>'> Notifications <i class="material-icons">notifications</i> </a></li>
+            <li> <a href='edit_account.php' class='<?= setActive('edit_profile', $pageOpt['navName']); ?>'> Edit Account <i class="material-icons">edit</i></a></li>
+            <li> <a href='logout.php'> Logout </a></li>
+        </ul>
+        <!--                   <li><a href="#" class='de-emphasis'><i class="material-icons">play_for_work</i>Logout</a></li> -->
+      </div>
+  </nav>
+  <div class='clear'></div>
+</header>
--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	
 	$pageOpt = array(
 		"title"			    =>	"Welcome", 
-		'navName' 		  	=> 	"home", 
+		'navName' 		  	=> 	"feed", 
 		'cssIncludes'	  	=>	" ", 
 		"jsIncludes"	 	=>	" ",
 	);
@@ -19,24 +19,7 @@
     <body>
       <?php include_once "app/views/header.php"; ?>
       <div class='view'> 
-        <header>
-          <nav>
-              <ul class='navigation-menu'>
-                  <li><a href="#"><i class="material-icons">featured_play_list</i> Feed </a></li>
-                  <li><a href='#'><i class="material-icons">search</i> Search </a> </li>
-                  <li><a href="#"><i class="material-icons">sentiment_very_satisfied</i> Friends </a> </li>
-                  <li><a href="#"><i class="material-icons">group</i>Groups</a></li>
-                  <li><a href="#" class='de-emphasis'><i class="material-icons">play_for_work</i>Logout</a></li>
-              </ul>
-              <div class='user-profile-tab'> 
-                <a href='#'> <i class="material-icons">account_circle</i> Jacob Dickinson </a>
-              </div>
-          </nav>
-          <div class='clear'></div>
-        </header>
-        <div class='information-menu'>
-        ABC
-        </div>
+          
       </div>
     	<?php include_once "app/views/scripts.php"; ?>
     </body>
