d54536953bfde38cd0c3d71498289c6d91e4f205
jacobd1808 <jacobd1808@yahoo.co.uk>
2016-04-04 03:45:21 +0100

    some changes

--- index.php
+++ index.php
@@ -22,14 +22,34 @@
       <?php if ($profile_info['latitude'] == 0 && $profile_info['longitude'] == 0){ ?>
          <!--<div class='default-popup' data-content='searchPreferences' data-title='Welcome to FitConnect .. Please set your search preferences'>  
          -->
-
       <?php }  ?>
+      <div class='m-25'>
+        <div class='feed modulated-box'> 
+          <h2> FitConnect Feed </h2>
+          <?php for($i = 0; $i < 17; $i++ ) { ?>
+          <div class='feed-post'> 
+            <div class='feed-post-title'>
+              <img src='http://i.imgur.com/HQ3YU7n.gif' alt='avatar' class='user-avatar feed-avatar' />
+              <span> Name </span>
+            </div>
+            <div class='feed-post'>
+              Some text text text text 
+            </div>
           </div>
+          <? } ?>
+        </div>
+        <div class='feed-notices'> 
+          <div class='modulated-box'>
+            <h2> Recommendations</h2>
+            Works 
+          </div>
+          <div class='modulated-box'>
+            <h2> Freind Requests </h2>
+            Works 
+          </div>
+        </div>
       </div>
-    	<?php include_once "app/views/scripts.php"; ?>
-      <script>
-                // Click event function 
 
-      </script>
+    	<?php include_once "app/views/scripts.php"; ?>
     </body>
 </html>
